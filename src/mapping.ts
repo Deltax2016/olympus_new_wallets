a8a06a9666ef839a0a9c5884ec46b3466a7d14e4
Deltax2016 <deltaxussr@gmail.com>
2021-10-04 17:32:31 +0300

    events only

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,84 +1,72 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { Address, BigDecimal, BigInt, log } from '@graphprotocol/graph-ts'
 import {
   wOHM,
   Approval,
-  OwnershipTransferred,
-  TWAPEpochChanged,
-  TWAPOracleChanged,
-  TWAPSourceAdded,
-  TWAPSourceRemoved,
   Transfer
 } from "../generated/wOHM/wOHM"
-import { ExampleEntity } from "../generated/schema"
+//import { ExampleEntity } from "../generated/schema"
+import { Transfer as TransferOHM } from '../generated/schema'
+import { Balance } from '../generated/schema'
+import {SOHM_ERC20_CONTRACT, OHM_ERC20_CONTRACT} from './utils/Constants'
 
+/*
 export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
+
   let entity = ExampleEntity.load(event.transaction.from.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
   if (!entity) {
     entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
     entity.count = BigInt.fromI32(0)
   }
 
-  // BigInt and BigDecimal math are supported
   entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
   entity.owner = event.params.owner
   entity.spender = event.params.spender
-
-  // Entities can be written to the store with `.save()`
   entity.save()
 
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
+}*/
 
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.DOMAIN_SEPARATOR(...)
-  // - contract.PERMIT_TYPEHASH(...)
-  // - contract.allowance(...)
-  // - contract.approve(...)
-  // - contract.balanceOf(...)
-  // - contract.decimals(...)
-  // - contract.decreaseAllowance(...)
-  // - contract.increaseAllowance(...)
-  // - contract.name(...)
-  // - contract.nonces(...)
-  // - contract.owner(...)
-  // - contract.setVault(...)
-  // - contract.symbol(...)
-  // - contract.totalSupply(...)
-  // - contract.transfer(...)
-  // - contract.transferFrom(...)
-  // - contract.twapEpochPeriod(...)
-  // - contract.twapOracle(...)
-  // - contract.vault(...)
+export function handleTransfer(event: Transfer): void {
+  let entity = TransferOHM.load(event.transaction.hash.toHex())
+  let balances_from = createWallet(event.params.from)
+  let balances_to = createWallet(event.params.to)
+
+  if (!entity) {
+    entity = new TransferOHM(event.transaction.hash.toHex())
+  }
+
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.amount = event.params.value
+  entity.save()
+
+
 }
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+function createWallet(address: Address): Balance {
 
-export function handleTWAPEpochChanged(event: TWAPEpochChanged): void {}
+  let entity = Balance.load(address.toHex())
 
-export function handleTWAPOracleChanged(event: TWAPOracleChanged): void {}
+  if (!entity) {
+    entity = new Balance(address.toHex())
+  }
 
-export function handleTWAPSourceAdded(event: TWAPSourceAdded): void {}
+  let ohmContract = wOHM.bind(Address.fromString(OHM_ERC20_CONTRACT))
 
-export function handleTWAPSourceRemoved(event: TWAPSourceRemoved): void {}
+  entity.ohmBalance = toDecimal(ohmContract.balanceOf(Address.fromString(entity.id.toString())),9)
+  entity.address = address
+  entity.save()
 
-export function handleTransfer(event: Transfer): void {}
+  return entity as Balance
+}
+
+function toDecimal(
+  value: BigInt,
+  decimals: number = DEFAULT_DECIMALS,
+): BigDecimal {
+  let precision = BigInt.fromI32(10)
+    .pow(<u8>decimals)
+    .toBigDecimal();
+
+  return value.divDecimal(precision);
+}
